99cd50190537c47c4b94151eb2b5050c1e1268c8
Fernando Odilon <fernando.odilon@aluno.senai.br>
2026-02-20 08:48:35 -0300

    MODIFY: Fatoração das funções

--- js/app.js
+++ js/app.js
@@ -3,104 +3,77 @@ const resultado = document.getElementById('resultado');
 const input_01 = document.getElementById('valor01');
 const input_02 = document.getElementById('valor02');
 
-function calcularTotal(){
-    const v1 = parseFloat(input_01.value); 
-    const v2 = parseFloat(input_02.value);
 
-    if (isNaN(v1) || isNaN(v2)){
+function verificar() {
+    const valor01 = parseFloat(input_01.value); 
+    const valor02 = parseFloat(input_02.value);
+
+    if (isNaN(valor01) || isNaN(valor02)){
         resultado.style.display = 'flex'
         resultado.innerHTML = `
         <p>Valor 01 ou Valor 02 inválido. Digite um número positivo</p>
         `;
+        return;
     }
-    else {
-        resultado.style.display = 'flex';
-        resultado.innerHTML = `
+    return {valor01, valor02};
+};
+
+
+function calcularTotal(){
+    const {v1, v2} = verificar();
+
+    resultado.style.display = 'flex';
+    resultado.innerHTML = `
         <h2>Total Calculado:</h2>
         <p>Você está adquirindo ${v2} unidades com valor unitário de R$ ${v1.toFixed(2)}</p>
         <h1>Valor total da compra: <span>R$ ${(v1 * v2).toFixed(2)}</span></h1>
         `;
-    };
 };
 
 function desconto(){
-    const v1 = parseFloat(input_01.value);
-    const v2 = parseFloat(input_02.value);
+    const {v1, v2} = verificar();
 
-    if (isNaN(v1) || isNaN(v2)){
-        resultado.style.display = 'flex'
-        resultado.innerHTML = `
-        <p>Valor 01 ou Valor 02 inválido. Digite um número positivo</p>
-        `;
-    }
-    else {
-        resultado.style.display = 'flex';
-        resultado.innerHTML = `
+    resultado.style.display = 'flex';
+    resultado.innerHTML = `
         <h2>Desconto Calculado:</h2>
         <p>Foi aplicado um desconto de ${v2}% sobre o valor de R$ ${v1.toFixed(2)}</p>
         <h1>Valor final com desconto: <span>R$ ${(v1 - ((v1 * v2) /100)).toFixed(2)}</span></h1>
         `;
     };
-};
 
 function juros(){
-    const v1 = parseFloat(input_01.value);
-    const v2 = parseFloat(input_02.value);
+    const {v1, v2} = verificar();
 
-    if (isNaN(v1) || isNaN(v2)){
-        resultado.style.display = 'flex'
-        resultado.innerHTML = `
-        <p>Valor 01 ou Valor 02 inválido. Digite um número positivo</p>
-        `;
-    }
-    else {
-        resultado.style.display = 'flex';
-        resultado.innerHTML = `
+    resultado.style.display = 'flex';
+    resultado.innerHTML = `
         <h2>Juros Calculado:</h2>
         <p>Foi aplicado um acréscimo de ${v2}% sobre o valor de R$ ${v1.toFixed(2)}</p>
         <h1>Valor final com juros: <span>R$ ${(((v1 * v2) / 100) + v1).toFixed(2)}</span></h1>
         `;
     };
-};
+
 
 function comissao(){
-    const v1 = parseFloat(input_01.value);
-    const v2 = parseFloat(input_02.value);
+    const {v1, v2} = verificar();
 
-    if (isNaN(v1) || isNaN(v2)){
-        resultado.style.display = 'flex'
-        resultado.innerHTML = `
-        <p>Valor 01 ou Valor 02 inválido. Digite um número positivo</p>
-        `;
-    }
-    else {
-        resultado.style.display = 'flex';
-        resultado.innerHTML = `
+    resultado.style.display = 'flex';
+    resultado.innerHTML = `
         <h2>Comissão Calculada:</h2>
         <p>Comissão de ${v2}% sobre uma venda de R$ ${v1.toFixed(2)}</p>
         <h1>Valor de comissão: <span>R$ ${((v1 * v2) / 100).toFixed(2)}</span></h1>
         `;
-    };  
-};
+};  
+
 
 function lucro(){
-    const v1 = parseFloat(input_01.value);
-    const v2 = parseFloat(input_02.value);
+    const {v1, v2} = verificar();
 
-    if (isNaN(v1) || isNaN(v2)){
-        resultado.style.display = 'flex'
-        resultado.innerHTML = `
-        <p>Valor 01 ou Valor 02 inválido. Digite um número positivo</p>
-        `;
-    }
-    else {
-        resultado.style.display = 'flex';
-        resultado.innerHTML = `
+    resultado.style.display = 'flex';
+    resultado.innerHTML = `
         <h2>Lucro Calculado:</h2>
-        <p>Preço de venda: R$ ${v1.toFixed(2)} | Custo: R$ ${v2.toFixed(2)}</p>
+        <p>Preço de Venda: R$ ${v1.toFixed(2)} | Custo: R$ ${v2.toFixed(2)}</p>
         <h1>Resultado financeiro: <span>R$ ${(v1 - v2).toFixed(2)}</span></h1>
         `;
-    };  
 };
 
 function limpar(){
